65c419c9e12246e902b0bcf9e37379e222c6e9ee
ThongNguyen <nguyenminhthong1994@gmail.com>
2023-06-18 14:35:52 +0700

    first commit

---
+++ src/main/java/com/learning/firstprogram/FirstProgramApplication.java
@@ -0,0 +1,17 @@
+package com.learning.firstprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProgramApplication.class, args);
+	}
+	public String welcome(){
+		return "Welcome dude!!";
+	}
+}
